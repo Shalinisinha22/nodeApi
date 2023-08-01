f3178d15a9eafafc05a1bc132b1c84d0bd8c23ff
Shalinisinha22 <shalinisinha.cspatna@gmail.com>
2023-08-01 22:30:35 +0530

    commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+const cors=require("cors")
+
+
+const app = express();
+app.use(cors())
+const port =process.env.PORT || 3000;
+
+// Set the static folder to serve images
+app.use(express.static(path.join(__dirname, 'images')));
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Multer storage configuration to upload files (if required)
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, 'images'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
+// API endpoint to retrieve JSON data containing image references
+app.get('/api/images', (req, res) => {
+  const data = require('./data.json');
+  res.json(data);
+});
+
+// API endpoint to upload an image (if required)
+app.post('/api/upload', upload.single('image'), (req, res) => {
+  res.json({ message: 'Image uploaded successfully!' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
